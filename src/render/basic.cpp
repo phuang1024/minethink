78f1bf0d082344d797cf1135ced8656e4d62c96f
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-09-20 22:43:05 -0700

    3D space classes.

---
+++ src/render/basic.cpp
@@ -0,0 +1,33 @@
+//
+//  Minethink
+//  3D block based game.
+//  Copyright Patrick Huang 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/*
+ This engine is called "basic".
+ It calculates the brightness of each face,
+ and renders the whole face with that color.
+ Ambient Occlusion may be added in the future.
+*/
+
+
+
+namespace Render {
+
+
+
+}  // namespace Render
---
+++ src/space3d/space3d.hpp
@@ -0,0 +1,48 @@
+//
+//  Minethink
+//  3D block based game.
+//  Copyright Patrick Huang 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <vector>
+
+
+namespace Space3D {
+
+/**
+ * 3D point.
+ * Uses floats to save memory.
+ */
+struct Point {
+    float x, y, z;
+};
+
+/**
+ * Triangle in 3D space
+ */
+struct Tri {
+    Point p1, p2, p3;
+};
+
+/**
+ * Mesh
+ * Collection of triangles
+ */
+struct Mesh {
+    std::vector<Tri> tris;
+};
+
+}  // namespace Space3D
